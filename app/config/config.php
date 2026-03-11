c1a5fe079a6e5995ac0099c64c994fc6256f96f0
unknown <abdullahalswalmi@gmail.com>
2026-03-11 02:36:52 +0200

    update: updating port env variable

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_PORT', '3307');
+define('DB_PORT', '3307'); // add ur port 
 define('DB_NAME', 'cafeteria_db');
 define('DB_USER', 'root');
 define('DB_PASS', '');
