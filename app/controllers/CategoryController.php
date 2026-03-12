9b30d13f14d9464943e003c144662be0f58f845a
unknown <abdullahalswalmi@gmail.com>
2026-03-13 01:44:13 +0200

    feat: added category controller

---
+++ app/controllers/CategoryController.php
@@ -0,0 +1,67 @@
+<?php
+require_once __DIR__ . "/../models/Category.php";
+
+class CategoryController{
+
+public Category $categoryModel;
+
+function __construct(){
+$this->categoryModel = new Category();
+}
+
+function createCategory(){
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+$name = trim($_POST['name']) ?? '';
+
+
+
+
+if(empty($name)){
+    echo "empty";
+    $error = "Category name Can't be Empty!";
+    require_once __DIR__ . '/../views/admin/addCategory.php';
+    return;
+
+
+}         
+
+
+if(strlen($name) > 100){
+    $error = "Name must be less than 100 characters";
+    require_once __DIR__ . '../views/admin/addCategory.php';
+    return;
+
+}
+
+if($this->categoryModel->create($name)){
+    header("Location: /PHP-CAFETERA/app/views/admin/add_product.php?msg=category_Added");
+    exit();
+}
+
+else{
+    header("Location: /PHP-CAFETERA/app/views/admin/add_product.php?msg=category_exists");
+    
+}
+
+}
+
+
+
+
+
+}
+
+function showAllCategories(){
+
+$all = $this->categoryModel->getAll();
+return $all;
+}
+
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $controller = new CategoryController();
+    $controller->createCategory();
+}
